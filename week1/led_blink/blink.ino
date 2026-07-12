34a16bc662edda0f38b832617833919e5708f979
anjalisinha230680-cmyk <anjalisinha29032010@gmail.com>
2026-07-12 16:56:00 +0530

    feat : v1.2 control blink speed using potentiometer

--- week1/led_blink/blink.ino
+++ week1/led_blink/blink.ino
@@ -1,13 +1,15 @@
 int count = 0;
+int potPin = A0;
 void setup(){
  pinMode(13,OUTPUT);
  Serial.begin(9600);
 }
 void loop(){
+  int delayTime = analogRead(potPin);
   digitalWrite(13,HIGH);
-  delay(500);
+  delay(delayTime);
   digitalWrite(13,LOW);
-  delay(500);
+  delay(delayTime);
 
  count++;
  Serial.print("Blink count:");
