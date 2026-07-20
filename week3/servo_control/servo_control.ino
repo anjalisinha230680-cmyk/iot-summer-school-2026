f1b43a2ef5e42c0a5c725093afdddb83e85924da
anjalisinha230680-cmyk <anjalisinha29032010@gmail.com>
2026-07-20 20:58:27 +0530

    feat: add servo motor control with potentiometer

---
+++ week3/servo_control/servo_control.ino
@@ -0,0 +1,43 @@
+#include <Servo.h>
+
+Servo myServo;
+
+const int potPin = A0;
+const int buttonPin = 2;
+const int servoPin = 9;
+
+void setup() {
+  myServo.attach(servoPin);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  int potValue = analogRead(potPin);
+
+  int angle = map(potValue, 0, 1023, 0, 180);
+
+  myServo.write(angle);
+
+  Serial.print("Servo Angle: ");
+  Serial.print(angle);
+  Serial.println("°");
+
+  if (digitalRead(buttonPin) == LOW) {
+
+    for (int i = 0; i <= 180; i++) {
+      myServo.write(i);
+      delay(10);
+    }
+
+    for (int i = 180; i >= 0; i--) {
+      myServo.write(i);
+      delay(10);
+    }
+  }
+
+  delay(100);
+}
